d0a0a7dd4988e3b84b62af1a139b4e2ee12a65e7
Ultra <valentin.ganchev@gmail.com>
2013-03-03 16:43:40 +0200

    First commit from home

---
+++ src/logging/LoggingDemo.java
@@ -0,0 +1,12 @@
+package logging;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoggingDemo {
+    WebDriver driver;
+    public void init(){
+        driver=new ChromeDriver();
+    }
+}
