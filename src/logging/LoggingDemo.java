43792f7c2762a6aa641ddfbb79d6e37a756a0b9d
valentin <valentin.ganchev@gmail.com>
2013-03-04 16:39:45 +0200

    Commit from Office #2

--- src/logging/LoggingDemo.java
+++ src/logging/LoggingDemo.java
@@ -2,11 +2,38 @@ package logging;
 
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.logging.*;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.ListIterator;
+import java.util.logging.Level;
 
 public class LoggingDemo {
+    DesiredCapabilities caps = DesiredCapabilities.firefox();
+    LoggingPreferences logPref = new LoggingPreferences();
     WebDriver driver;
-    public void init(){
-        driver=new ChromeDriver();
+    @Test
+    public void TestMethod(){
+
+        logPref.enable(LogType.DRIVER, Level.ALL);
+        caps.setCapability(CapabilityType.LOGGING_PREFS, logPref);
+        driver = new FirefoxDriver(caps);
+        Logs log =  driver.manage().logs();
+        LogEntries entries = log.get(LogType.PROFILER);
+        driver.get("http://www.google.com");
+        List<LogEntry> entry=entries.getAll();
+        ListIterator<LogEntry> iterator=entry.listIterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        System.out.println(driver.getTitle());
+        Assert.assertEquals(driver.getTitle(), "Google");
+        driver.quit();
     }
+
 }
