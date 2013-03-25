eb6f0a4eb0791972ede0ef69ba2c13c809ae9e06
Ultra <valentin.ganchev@gmail.com>
2013-03-25 11:38:04 +0200

    Added JBehave skeleton

---
+++ src/jbehave/JBehaveDemo.java
@@ -0,0 +1,11 @@
+package jbehave;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Valentin
+ * Date: 3/25/13
+ * Time: 4:56 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JBehaveDemo {
+}
---
+++ src/jbehave/LoginStepDefinition.java
@@ -0,0 +1,20 @@
+package jbehave;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.When;
+import org.jbehave.core.annotations.Then;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Valentin
+ * Date: 3/25/13
+ * Time: 5:19 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LoginStepDefinition {
+
+    @Given("Login page is loaded")
+    public void loadLoginpage(){
+
+    }
+}
